4b95c54c005930d8f3c3373340c4dd27ffff3592
andre-0303 <bandeiradev.ti@gmail.com>
2025-04-17 21:54:07 -0300

    Ajustando o banner

--- src/components/banner/page.tsx
+++ src/components/banner/page.tsx
@@ -1,23 +1,27 @@
-    import Image from 'next/image';
+import Image from 'next/image';
 
-    export default function Banner() {
+export default function Banner() {
     return (
         <div className="relative w-full h-[300px] md:h-[500px]">
-        <Image
-            src="/banner.jpg" 
-            alt="banner"
-            layout="fill"
-            objectFit="cover"
-            quality={100}
-            priority
-        />
+            <Image
+                src="/banner.jpg"
+                alt="banner"
+                layout="fill"
+                objectFit="cover"
+                quality={100}
+                priority
+            />
 
-        <div className="absolute inset-0 bg-blue-600 opacity-40"></div>
+            <div className="absolute inset-0 bg-blue-600 opacity-40"></div>
 
-        <div className="absolute inset-0 flex items-center justify-center flex-col text-white text-4xl font-bold">
-            <h1 className='text-xl sm:text-2xl md:text-4xl font-extrabold'>Curso Técnico em Informática</h1>
-            <h4 className='text-sm sm:text-base md:text-xl font-normal mt-2'>Tecnologia, inovação e futuro começam aqui</h4>
-        </div>
+            <div className="absolute inset-0 flex items-center justify-center flex-col text-white text-center">
+                <h1 className='text-2xl sm:text-3xl md:text-5xl font-extrabold'>
+                    Curso Técnico em Informática
+                </h1>
+                <h4 className='text-base sm:text-lg md:text-xl font-normal mt-2'>
+                    Tecnologia, inovação e futuro começam aqui
+                </h4>
+            </div>
         </div>
     );
-    }
+}
